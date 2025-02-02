74b67917813f1daf633f58d28205af521e0f3f86
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2025-02-02 18:39:28 +0100

    orders localhost only

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     # Service URLs (to be configured later)
     CUSTOMER_SERVICE_URL: str = "http://localhost:3000"
     KITCHEN_SERVICE_URL: str = "http://localhost:3001"
-    DELIVERY_SERVICE_URL: str = "http://localhost:3002"
-    ORDER_SERVICE_URL: str = "http://localhost:3003"
+    ORDER_SERVICE_URL: str = "http://localhost:3002"
+    DELIVERY_SERVICE_URL: str = "http://localhost:3003"
     
     class Config:
         env_file = ".env"
